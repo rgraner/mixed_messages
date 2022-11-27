5098ae8bd24e1730669260ba8070a08c09d195c7
rgraner <ricardograner@gmail.com>
2022-11-27 17:03:31 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const drinks = ['water', 'wine', 'coca-cola', 'vodka']
+const actions = ['danced', 'kissed', 'laughed', 'made love']
+const complement = ['a princess', 'a machine', 'the best', 'the only one']
+
+
