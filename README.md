55c969a2992193caa1e7f80ff211c535f5e74ff9
rgraner <ricardograner@gmail.com>
2022-11-27 17:07:21 +0000

    Make random sentences and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Mixed Messages (Codecademy)
 
+"Every time a user runs a program, they should get a new, randomized output..." (Codecademy)
--- script.js
+++ script.js
@@ -2,4 +2,13 @@ const drinks = ['water', 'wine', 'coca-cola', 'vodka']
 const actions = ['danced', 'kissed', 'laughed', 'made love']
 const complement = ['a princess', 'a machine', 'the best', 'the only one']
 
+let randomIdx1 = Math.floor(Math.random() * drinks.length)
+let randomIdx2 = Math.floor(Math.random() * actions.length)
+let randomIdx3 = Math.floor(Math.random() * complement.length)
 
+let drink = drinks[randomIdx1]
+let action = actions[randomIdx2]
+
+console.log(`She wants ${drink}!`)
+console.log(`She drank ${drink} and ${action}!`)
+console.log(`She drank ${drink} and ${action} as if she was ${complement[randomIdx3]}!`)
